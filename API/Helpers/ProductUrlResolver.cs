d9c3a9abf4f14526cbf4844699ec59c2b9a0b382
wendelguimaraes <wendelguimaraes@orbcom.pt>
2024-02-12 16:18:16 +0000

    End of section 4

---
+++ API/Helpers/ProductUrlResolver.cs
@@ -0,0 +1,24 @@
+using API.Dtos;
+using AutoMapper;
+using Core.Entities;
+
+namespace API.Helpers
+{
+    public class ProductUrlResolver : IValueResolver<Product, ProductToReturnDto, string>
+    {
+        public IConfiguration _config { get; }
+        public ProductUrlResolver(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public string Resolve(Product source, ProductToReturnDto destination, string destMember, ResolutionContext context)
+        {
+            if(!string.IsNullOrEmpty(source.PictureUrl)){
+                return _config["ApiUrl"] + source.PictureUrl;
+            }
+
+            return null;
+        }
+    }
+}
